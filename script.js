7bfeb553192b6b80ad078827225827bbc1257786
Deepashri Dali <82769106+DeepashriDali@users.noreply.github.com>
2023-03-12 22:52:43 +0000

    make nav-bar responsive

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("check");
+//Nav-bar mobile
+let hamburger = document.querySelector(".hamburger");
+let navBar = document.querySelector(".nav-bar");
+
+hamburger.addEventListener("click", () => {
+  console.log("hi");
+  navBar.classList.toggle("active");
+});
