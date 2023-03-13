8f59450a4151b17d65fc88a1f620df82156bd078
Deepashri Dali <82769106+DeepashriDali@users.noreply.github.com>
2023-03-13 12:52:40 +0000

    add dark and light mode function

--- script.js
+++ script.js
@@ -2,8 +2,19 @@ console.log("check");
 //Nav-bar mobile
 let hamburger = document.querySelector(".hamburger");
 let navBar = document.querySelector(".nav-bar");
-
+let modebtn = document.querySelector(".mode");
 hamburger.addEventListener("click", () => {
   console.log("hi");
   navBar.classList.toggle("active");
+});
+
+//dark and light mode
+let switchMode = document.querySelector(".mode");
+switchMode.addEventListener("click", () => {
+  document.body.classList.toggle("light-mode");
+  if (modebtn.innerHTML === "Light Mode") {
+    modebtn.innerHTML = "Dark Mode";
+  } else {
+    modebtn.innerHTML = "Light Mode";
+  }
 });
